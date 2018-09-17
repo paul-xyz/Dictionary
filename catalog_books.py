714c24f4ef44bf7811193f070acc9d750f42faa8
Paul Michael Brown <paul.brown@stu.jefferson.kyschools.us>
2018-09-17 08:30:05 -0400

    first commit

---
+++ catalog_books.py
@@ -0,0 +1,9 @@
+catalog = []
+
+def book(title, author, genre):
+    book_add = input ("Do you want to put this book in the catalog? ")
+    if book_add == ("yes, y, Yes"):
+        catalog.append[]:
+        return "Book has been added to the catalog, call the variable catalog to view books that are in the catalog."
+    else:
+        return;
